7ee0b17c1514afbbf6e9112868f23f8a4c8739f8
orlann <orlann@ukr.net>
2020-12-15 14:07:54 +0200

    finished lab 8.1(2)

---
+++ lab8.1(2)/1.cpp
@@ -0,0 +1,50 @@
+//ітераційний спосіб
+#include <iostream>
+using namespace std;
+
+int count(char* charSequence)
+{
+	int counter = 0;
+	for (int i = 1; i < strlen(charSequence); i++)
+	{
+		if (charSequence[i] == 'a' || charSequence[i] == 'b' || charSequence[i] == 'c')
+		{
+			if (charSequence[i + 1] == 'a' || charSequence[i + 1] == 'b' || charSequence[i + 1] == 'c')
+			{
+				if (charSequence[i - 1] == 'a' || charSequence[i - 1] == 'b' || charSequence[i - 1] == 'c')
+				{
+					counter++;
+				}
+			}
+		}
+	}
+	return counter;
+}
+char* modifying(char* charSequence)
+{
+	for (int i = 1; i < strlen(charSequence); i++)
+	{
+		if (charSequence[i] == 'a' || charSequence[i] == 'b' || charSequence[i] == 'c')
+		{
+			if (charSequence[i + 1] == 'a' || charSequence[i + 1] == 'b' || charSequence[i + 1] == 'c')
+			{
+				if (charSequence[i - 1] == 'a' || charSequence[i - 1] == 'b' || charSequence[i - 1] == 'c')
+				{
+					charSequence[i] = '*';
+					charSequence[i - 1] = '*';
+					charSequence[i + 1] = '*';
+				}
+			}
+		}
+	}
+	return charSequence;
+}
+
+int main()
+{
+	char charSequence[100];
+	cout << "Enter string: " << endl;
+	cin.getline(charSequence, 100);
+	cout << "Number of letter combinations : " << count(charSequence)<< endl;
+	cout << "Modified string: " << modifying(charSequence);
+}
